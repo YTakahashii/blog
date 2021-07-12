8094e673ca93e9d9b256d2a0187f89774024aedc
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2021-07-12 22:42:01 +0900

    fix: image path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const PROJECT_NAME = '/tech-blog';
 const isProd = process.env.NODE_ENV === 'production';
+const STATIC_FOLDER = isProd ? PROJECT_NAME : '';
 
 module.exports = {
-  assetPrefix: isProd ? PROJECT_NAME : '',
+  assetPrefix: STATIC_FOLDER,
+  basePath: STATIC_FOLDER,
+  staticFolder: STATIC_FOLDER,
 };
--- src/components/markdowns/_components/img.tsx
+++ src/components/markdowns/_components/img.tsx
@@ -1,11 +1,16 @@
 import { ComponentPropsWithoutRef } from 'react';
 import { Image } from '@chakra-ui/react';
+import getConfig from 'next/config';
 
 type Props = ComponentPropsWithoutRef<'img'>;
+
+const {
+  publicRuntimeConfig: { staticFolder },
+} = getConfig();
 
 export const img: React.VFC<Props> = ({ src, width, height, ...rest }) => {
   const relativePublicPath = '../public' as const;
   const nextSrc = src.startsWith(relativePublicPath) ? src.substring(relativePublicPath.length) : src; // /assets から始まるnextが解釈可能なパスに変換
 
-  return <img src={nextSrc} width={700} height={394} />;
+  return <img src={`${staticFolder}${nextSrc}`} width={700} height={394} />;
 };
