20c889d1f4344dd7f1a49d159c802055fb53cd7f
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2021-07-12 21:37:58 +0900

    fix: pagesの画像テスト

--- src/components/markdowns/_components/img.tsx
+++ src/components/markdowns/_components/img.tsx
@@ -1,14 +1,15 @@
 import { ComponentPropsWithoutRef } from 'react';
 import { Image } from '@chakra-ui/react';
+
 type Props = ComponentPropsWithoutRef<'img'>;
 
 export const img: React.VFC<Props> = ({ src, width, height, ...rest }) => {
   const relativePublicPath = '../public' as const;
-  const nextSrc = src.startsWith(relativePublicPath) ? src.substring(relativePublicPath.length) : src; // ローカルの md エディタでも画像を表示したいためパスを変換
+  const nextSrc = src.startsWith(relativePublicPath) ? src.substring(relativePublicPath.length) : src; // /assets から始まるnextが解釈可能なパスに変換
 
   return (
     <div style={{ display: 'grid', placeItems: 'center' }}>
-      <Image src={nextSrc} width={700} height={394} />
+      <Image src={process.env.NODE_ENV === 'production' ? `.${nextSrc}` : nextSrc} width={700} height={394} />
     </div>
   );
 };
