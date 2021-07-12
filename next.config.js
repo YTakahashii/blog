56426be18000602b4ca42250dc986dd39a768f17
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2021-07-12 22:14:27 +0900

    fix: basePathを設定

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
+const PROJECT_NAME = 'tech-blog';
+
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_BUILD) {
-    return { ...defaultConfig, assetPrefix: 'tech-blog' };
+    return { ...defaultConfig, assetPrefix: PROJECT_NAME, basePath: PROJECT_NAME };
   }
   return defaultConfig;
 };
--- src/components/markdowns/_components/img.tsx
+++ src/components/markdowns/_components/img.tsx
@@ -7,9 +7,5 @@ export const img: React.VFC<Props> = ({ src, width, height, ...rest }) => {
   const relativePublicPath = '../public' as const;
   const nextSrc = src.startsWith(relativePublicPath) ? src.substring(relativePublicPath.length) : src; // /assets から始まるnextが解釈可能なパスに変換
 
-  return (
-    <div style={{ display: 'grid', placeItems: 'center' }}>
-      <Image src={process.env.NODE_ENV === 'production' ? `/tech-blog${nextSrc}` : nextSrc} width={700} height={394} />
-    </div>
-  );
+  return <img src={nextSrc} width={700} height={394} />;
 };
