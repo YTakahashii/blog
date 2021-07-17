797b0d9ba08f7c7b16249507978dd291cf30b0c4
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2021-07-17 16:43:07 +0900

    chore: 環境変数から読み込み

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const PROJECT_NAME = '/tech-blog';
-const isProd = process.env.NODE_ENV === 'production';
-const STATIC_FOLDER = isProd ? PROJECT_NAME : '';
-
 module.exports = {
-  assetPrefix: STATIC_FOLDER,
-  basePath: STATIC_FOLDER,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
 };
