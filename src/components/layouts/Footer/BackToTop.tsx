9275ed72d7c4d9db155593c1aec588df7a2adaa0
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2021-08-01 17:43:23 +0900

    fix: contrast

--- src/components/layouts/Footer/BackToTop.tsx
+++ src/components/layouts/Footer/BackToTop.tsx
@@ -6,7 +6,7 @@ export const BackToTop: React.VFC = () => (
   <Box>
     <Divider />
     <Box py={6}>
-      <InternalLink href="/" color="blue.500">
+      <InternalLink href="/" color="linkedin.600">
         <ArrowBackIcon mr="2px" />
         最新の投稿一覧に戻る
       </InternalLink>
--- src/components/markdowns/_components/a.tsx
+++ src/components/markdowns/_components/a.tsx
@@ -1,7 +1,6 @@
 import { ComponentPropsWithoutRef } from 'react';
 import NextLink from 'next/link';
 import { Link } from '@chakra-ui/react';
-import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 type Props = ComponentPropsWithoutRef<'a'>;
 
@@ -11,7 +10,7 @@ export const a: React.FC<Props> = ({ children, href }) =>
       <Link textDecoration="underline">{children}</Link>
     </NextLink>
   ) : (
-    <Link href={href} color="blue.500" textDecoration="underline" isExternal>
+    <Link href={href} color="linkedin.600" textDecoration="underline" isExternal>
       {children}
     </Link>
   );
