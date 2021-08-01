d32bebdd485c76259c3824641a65a4abb6a3fd80
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2021-08-01 17:53:00 +0900

    fix: basePath

--- src/components/layouts/Header/SiteIcon.tsx
+++ src/components/layouts/Header/SiteIcon.tsx
@@ -3,9 +3,9 @@ import NextLink from 'next/link';
 import { basePath } from 'src/constants/env';
 
 export const SiteIcon: React.VFC = () => (
-  <NextLink href={basePath}>
+  <NextLink href="/">
     <a>
-      <Image src="logo.svg" w="150px" h="auto" cursor="pointer" />
+      <Image src={`${basePath}/logo.svg`} w="150px" h="auto" cursor="pointer" />
     </a>
   </NextLink>
 );
