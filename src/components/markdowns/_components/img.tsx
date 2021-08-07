9cf45401e6a15cee271bf2c75a5d86111cf4759d
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2021-08-07 16:31:13 +0900

    fix: img alt を設定

--- src/components/markdowns/_components/img.tsx
+++ src/components/markdowns/_components/img.tsx
@@ -4,9 +4,9 @@ import { basePath } from 'src/constants/env';
 
 type Props = ComponentPropsWithoutRef<'img'>;
 
-export const img: React.VFC<Props> = ({ src, width, height }) => {
+export const img: React.VFC<Props> = ({ src, width, height, alt }) => {
   const relativePublicPath = '../public/' as const;
   const nextSrc = src.startsWith(relativePublicPath) ? src.substring(relativePublicPath.length) : src; // assets から始まるnextが解釈可能なパスに変換
 
-  return <SkeletonImage src={`${basePath}/${nextSrc}`} w={width} h={height} m="0 auto" />;
+  return <SkeletonImage src={`${basePath}/${nextSrc}`} w={width} h={height} alt={alt} aria-label={alt} m="0 auto" />;
 };
