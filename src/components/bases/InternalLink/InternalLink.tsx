dc20a406d330345169aabd272368da5cdb991926
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2021-08-09 18:19:10 +0900

    fix: aria-label

--- src/components/bases/InternalLink/InternalLink.tsx
+++ src/components/bases/InternalLink/InternalLink.tsx
@@ -2,7 +2,7 @@ import NextLink from 'next/link';
 import { Link, LinkProps } from '@chakra-ui/react';
 
 export const InternalLink: React.FC<LinkProps> = ({ children, href, ...rest }) => (
-  <NextLink href={href} passHref>
+  <NextLink href={href} aria-label={rest['aria-label']} passHref>
     <Link w="fit-content" outline="none" _hover={{ outline: 'none' }} {...rest}>
       {children}
     </Link>
