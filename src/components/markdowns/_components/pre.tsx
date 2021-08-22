6a7776b186dafe550a4dec2cf7cb44ccf9d68def
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2021-08-22 17:57:24 +0900

    test

--- src/components/markdowns/_components/pre.tsx
+++ src/components/markdowns/_components/pre.tsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 import { ComponentPropsWithoutRef } from 'react';
 
 export const pre: React.FC<ComponentPropsWithoutRef<'pre'>> = ({ children, className, style }) => (
-  <Box as="pre" p={2} my={5} borderRadius="md" overflowX="scroll" {...{ className, style }}>
+  <Box as="pre" p={2} my={5} borderRadius="md" overflowX="scroll" w="100%" {...{ className, style }}>
     {children}
   </Box>
 );
